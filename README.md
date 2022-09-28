695120b4f8c8a3838deb6ade4317b4d69ff30a59
a1085728420 <1085728420@qq.com>
2022-09-28 11:39:29 +0800

    add convit.py

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # mindcv_convit
 This repo is to implement convit using MindSpore
+
+# Finish
+convit.py -> 能够跑通main测试函数
+
+# TO DO
+1、model.train 方式训练
+2、pynative+混合编程训练
+3、差异文档分析编写
---
+++ models/layers/mlp.py
@@ -0,0 +1,27 @@
+""" MLP module w/ dropout and configurable activation layer
+"""
+
+from mindspore import nn
+
+class Mlp(nn.Cell):
+    """MLP Cell"""
+
+    def __init__(self, in_features, hidden_features=None,
+                 out_features=None,
+                 act_layer=nn.GELU,
+                 drop=0.):
+        super().__init__()
+        out_features = out_features or in_features
+        hidden_features = hidden_features or in_features
+        self.fc1 = nn.Dense(in_channels=in_features, out_channels=hidden_features, has_bias=True)
+        self.act = act_layer()
+        self.fc2 = nn.Dense(in_channels=hidden_features, out_channels=out_features, has_bias=True)
+        self.drop = nn.Dropout(keep_prob=1.0 - drop)
+
+    def construct(self, x):
+        x = self.fc1(x)
+        x = self.act(x)
+        x = self.drop(x)
+        x = self.fc2(x)
+        x = self.drop(x)
+        return x
